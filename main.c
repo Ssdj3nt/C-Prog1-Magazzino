89556216bf2a28e255536604aad206ad46913bd4
francesco <francescopo@outlook.com>
2022-02-14 12:13:46 +0100

    BUGFIX

--- main.c
+++ main.c
@@ -26,23 +26,23 @@ void main()
             {
                     {1,"Samsung","Tv 55 pollici 8K",100,570},
                     {2,"Edison","Decoder DVB-T2",100,20},
-                    {3,"IPhone","PRO MAX",100,1250},
+                    {3,"IPhone 13 PRO MAX","512gb,6gb ram",100,1250},
                     {4,"AirPods","Chip H1",100,250},
-                    {5,"Apple Watch","Serie 7",100,400},
+                    {5,"Apple Watch 7","Res 396×484 pixel",100,400},
                     {6,"Huawei Matebook 13","Ryzen 7 3700U,16gb ram",100,750},
                     {7,"HP Omen 25L","10th Gen Intel Core i5,NVIDIA GeForce GTX 1650",100,800},
                     {8,"Razer Orochi","4000DPI",100,75},
                     {9,"Logitech G810 Orion Spectrum","Romer-G Switch,Rgb",100,100},
                     {10,"Creative Pebble v3","Bluetooth",100,40},
                     {11,"Xiaomi Mi 34 Curved","3440x1440 144hz 1ms",100,470},
-                    {12,"Hyper X Predator","32gb 4x4 3600mhz CL17",100,250},
+                    {12,"Kingston Hyper X Predator","32gb 4x4 3600mhz CL17",100,250},
                     {13,"ASUS RTX 3080 TUF","10GB VRAM",100,2500},
                     {14,"MSI B500 Mortar","AM4 chipset",100,140},
-                    {15,"Ryzen 5600X","4.65GHZ",100,370},
+                    {15,"AMD Ryzen 5600X","6/12 Thread,4.65GHZ",100,370},
                     {16,"Deepcool Macube 110","Micro-Atx",100,70},
                     {17,"Deepcool RM120","120mm RGB",100,50},
                     {18,"Corsair RM750X","750W",100,170},
-                    {19,"Western Digital 1tb","1Tb storage",100,50},
+                    {19,"Western Digital","1Tb storage",100,50},
                     {20,"Samsung 980 Pro","SSD 1TB",100,250}};
     int sel;
     printf("\n\n-Benvenuto in SAP Enterprise Resource Planning-\n-------------ELECTRONICS WAREHOUSE-------------\n");  //L'uso del nome 'SAP' e il suo marchio registrato sono utilizzati solo per scopi dimostrativi a fini didattici.
@@ -125,8 +125,8 @@ void acquisti(Oggetto merce[20])
                     merce[i].qty_stored = merce[i].qty_stored-ar;
                     guadagni_parz=(ar*(merce[i].euro));
                     printf("\n|Codice:%2d|--|Merce: %s|--|Qty: %d|--|Prezzo: %d euro|--|Guadagno delle vendite per articolo: %d Euro|:\n",merce[i].prod_cod,merce[i].name,merce[i].qty_stored,merce[i].euro,guadagni_parz);
+                    i++;
                 }
             }
-            i++;
         }
     }
